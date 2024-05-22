8e49c60ff9909ca3c811b67e5e031b917bb6db0f
HansiSumanasekara <98638381+IT20665616@users.noreply.github.com>
2024-05-22 11:33:41 +0530

    Recovering the website

--- website/src/App.js
+++ website/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";// import-react-router-dom
+//this is use to create navigation routes for pages
+
+import Header from "./Components/Header";
+import LiteritureSurvey from "./Components/LiteritureSurvey";
+import Home from "./Components/Home";
+import Docs from "./Components/Documents";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+
+      <Route path="/" element={<Home />} />
+      <Route path="/header" element={<Header />} />
+      <Route path="/ls" element={<LiteritureSurvey />} />
+      <Route path="/docs" element={<Docs />} />
+        
+
+
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
